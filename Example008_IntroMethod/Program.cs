a5e9bfb13a0777e5219e46ffe1aa9a7099cd8114
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-21 00:28:01 +0300

    add examples 008-011

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+  int result = arg1;
+  if (arg2>result) result=arg2;
+  if (arg3>result) result=arg3;
+  return result;
+}
+
+int a1 = 13;
+int b1 = 14;
+int c1 = 15;
+int a2 = 80;
+int b2 = 17;
+int c2 = 18;
+int a3 = 19;
+int b3 = 20;
+int c3 = 2991;
+
+int max1 = Max (a1,b1,c1);
+int max2 = Max (a2,b2,c2);
+int max3 = Max (a3,b3,c3);
+int max = Max (max1,max2,max3);
+
+Console.WriteLine(max1);
+Console.WriteLine(max2);
+Console.WriteLine(max3);
+Console.WriteLine(max);
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection) //объявляем массив collection
+// void -метод, - это метод, который ничего не возвращает. 
+// return в коде не должно быть
+{
+    int length = collection.Length; // поучаем длинну массива collection
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]=new Random().Next(1, 10);//обращаемся к аргументу collection
+        // на позицию index и кладем туда случайное целое число из диапазона 1-10.
+        index++;
+    }
+}
+void PrintArray(int[] col) // метод void, который печатает массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;// если искомого элемента нет, по умолчанию будет выдаваться
+    // позиция -4 
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index; // находим первую позицицию заданного числа
+            break; // преращаем поиск. останавливаем проверку
+        }
+        index++; //щелкаем индесы
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создаем новый массив, в котором 10 элементов
+FillArray(array); // заполняем массив
+array [4] = 4; // принудительно добавляем в массив четверку на позицию 4
+PrintArray(array); // распечатываем массив
+Console.WriteLine(); // выводим пустую строчку, чтобы отделить вывод массива
+
+int pos = IndexOf(array, 4); // определяем переменную pos, в нее кладем 
+//результат работы метода IndexOf. В качестве аргумента будет наш массив 
+// и ищем в массиве цифру 4 
+Console.WriteLine(pos);
